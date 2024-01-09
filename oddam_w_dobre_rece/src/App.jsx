c18bafbd00ba622abe8ee1b30ab76049f50be399
marrcelp <marcel.piaszczyk@gmail.com>
2024-01-10 00:09:48 +0100

    created home component with basic structure, added styles and home image

--- oddam_w_dobre_rece/src/App.jsx
+++ oddam_w_dobre_rece/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 function App() {
 
   return (
     <>
         <Header/>
+        <Home/>
     </>
   );
 }
---
+++ oddam_w_dobre_rece/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import homeimage from '../../src/assets/images/homeimage.png';
+
+const Home = () => {
+    return (
+        <>
+            <section className='home'>
+                <div className='banner'></div>
+                <div className='banner-description'>
+                    <h1>Zacznij pomagać! <br></br>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    <button>ODDAJ <br></br>RZECZY</button>
+                </div>
+            </section>
+
+        </>
+    );
+};
+
+export default Home;

bc699d1cd6576919a5e431e62556a06527b569b7
marrcelp <marcel.piaszczyk@gmail.com>
2024-01-11 23:23:21 +0100

    created Columns component with styling, divided main.scss for single components

---
+++ oddam_w_dobre_rece/src/components/Columns.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Columns = () => {
+    return (
+        <>
+        <section className='yellowColumns'>
+            <div className='columns-container'>
+                <div className='column'>
+                    <h1>10</h1>
+                    <h2>ODDANYCH WORKÓW</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolorum laudantium numquam optio quae quaerat sit sunt</p>
+                </div>
+                <div className='column'>
+                    <h1>5</h1>
+                    <h2>WSPARTYCH ORGANIZACJI</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolorum laudantium numquam optio quae quaerat sit sunt</p>
+                </div>
+                <div className='column'>
+                    <h1>7</h1>
+                    <h2>ZORGANIZOWANYCH ZBIÓREK</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolorum laudantium numquam optio quae quaerat sit sunt</p>
+                </div>
+            </div>
+        </section>
+        </>
+    );
+};
+
+export default Columns;

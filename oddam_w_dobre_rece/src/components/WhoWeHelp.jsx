15c149a3a9f048336ff3966dc829689aea856d27
marrcelp <marcel.piaszczyk@gmail.com>
2024-01-19 18:24:51 +0100

    extended WhoWeHelp component, added displaying organizations and local collecting, added pagination

--- oddam_w_dobre_rece/src/components/WhoWeHelp.jsx
+++ oddam_w_dobre_rece/src/components/WhoWeHelp.jsx
@@ -6,9 +6,10 @@ import { setPage } from '../reducer.js';
 
 const WhoWeHelp = () => {
     const [dataArray, setDataArray] = useState([]);
-    const [foundations, setFoundations] = useState([]);
+    const [helpers, setHelpers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = useSelector((state) => state.itemsPerPage);
+    const [displayType, setDisplayType] = useState('foundation');
 
 
     const dispatch = useDispatch();
@@ -17,7 +18,7 @@ const WhoWeHelp = () => {
         const fetchFoundations = async () => {
             try {
                 const { data, error } = await supabase
-                    .from('foundations')
+                    .from('helpers')
                     .select();
 
                 if (error) {
@@ -26,12 +27,14 @@ const WhoWeHelp = () => {
 
                 if (data) {
                     setDataArray(data);
+                    console.log('DANE', data)
+
                     const startIndex = (currentPage - 1) * itemsPerPage;
                     const endIndex = startIndex + itemsPerPage;
-                    setFoundations(data.slice(startIndex, endIndex));
+                    setHelpers(data.sort((a, b) => a.id - b.id).slice(startIndex, endIndex));
                     dispatch(setPage(currentPage));
                     console.log(data);
-                    console.log('Foundations length wynosi:', foundations.length)
+                    console.log('Foundations length wynosi:', helpers.length)
                 }
             } catch (error) {
                 console.error('Error during fetching data:', error.message);
@@ -56,27 +59,38 @@ const WhoWeHelp = () => {
         return pageNumbers;
     };
 
+    const renderHelpers = () => {
+        return (
+            <>
+                <ul>
+                    {helpers.map((helper) => (
+                        <div key={helper.id}>
+                            <li>{helper[displayType + '_name']}</li>
+                            <p>Cel i misja: {helper[displayType + '_description']}</p>
+                            <p>{helper[displayType + '_stuff']}</p>
+                            <br></br>
+                        </div>
+
+                    ))}
+                </ul>
+            </>
+        );
+    };
+
+
+
     return (
         <section className='whowehelp'>
             <div className='whowehelp-container'>
                 <h1>Komu pomagamy?</h1>
                 <img src={decoration_img} alt='decoration icon' className='decoration_img' />
                 <div className='buttons'>
-                    <button className='button'>Fundacjom</button>
-                    <button className='button'>Organizacjom pozarządowym</button>
-                    <button className='button'>Lokalnym zbiórkom</button>
+                    <button className='button' onClick={() => setDisplayType('foundation')}>Fundacjom</button>
+                    <button className='button' onClick={() => setDisplayType('organization')}>Organizacjom pozarządowym</button>
+                    <button className='button' onClick={() => setDisplayType('localcollections')}>Lokalnym zbiórkom</button>
                 </div>
                 <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
-                <ul>
-                    {foundations.map((foundation) => (
-                        <>
-                            <li key={foundation.id}>{foundation.name}</li>
-                            <p>Cel i misja: {foundation.description}</p>
-                            <p>{foundation.stuff}</p>
-                            <br></br>
-                        </>
-                    ))}
-                </ul>
+                {renderHelpers()}
                 <div className="pagination">
                     {renderPageNumbers()}
                 </div>

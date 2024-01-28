ee27dc4da72b92839232d096d40e02acf1c5281c
marrcelp <marcel.piaszczyk@gmail.com>
2024-01-28 22:18:21 +0100

    created validation to login component with styles

--- oddam_w_dobre_rece/src/components/Login.jsx
+++ oddam_w_dobre_rece/src/components/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import decoration_img from "../assets/icons/Decoration.svg";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+
+
+    function validateEmail(email) {
+        const [namePart, domainPart] = email.split('@');
+
+        const domainRegex = /^((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const isDomainValid = domainRegex.test(domainPart);
+
+        return isDomainValid;
+    }
+
+
+    function handleButtonClick() {
+
+        const isEmailValid = validateEmail(email);
+        const isPasswordValid = password.length >= 6;
+
+        setEmailError(!isEmailValid);
+        setPasswordError(!isPasswordValid);
+    }
+
+
+        return (
         <section className='login' id='login'>
             <div className='login-container'>
                 <h1>Zaloguj się</h1>
@@ -11,18 +38,20 @@ const Login = () => {
                 <div className='email_password'>
                     <label>
                         Email
-                        <input type="email" name="email" />
+                        <input type="email" name="email" onChange={(e) => setEmail(e.target.value)}/>
+                        {emailError ? <p className='invalid_data'>Niepoprawny format podanego e-maila!</p> : <p></p>}
                     </label>
                     <label>
                         Hasło
-                        <input type="password" name="password" />
+                        <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                        {passwordError ? <p className='invalid_data'>Hasło musi zawierać co najmniej 6 znaków!</p> : <p></p>}
                     </label>
                 </div>
                 <div className='login-buttons'>
                     <Link to='/register'>
                         <button>Załóż konto</button>
                     </Link>
-                    <button>Zaloguj się</button>
+                    <button onClick={handleButtonClick}>Zaloguj się</button>
                 </div>
             </div>
         </section>
